a06af57eabaeec69626c006ba0dcd63754ff329d
sdhutchins <sdhutchins@outlook.com>
2019-02-09 17:47:13 -0600

    Added columns to UI.

--- app.R
+++ app.R
@@ -10,9 +10,11 @@ source("helpers.R")
 
 # Define UI for application that logs into a server.
 ui <- navbarPage(
-  header = tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "https://fonts.googleapis.com/css?family=Cabin|Oxygen+Mono|Roboto+Mono|Share+Tech+Mono|Source+Code+Pro"),
-                     tags$style("* {font-family: 'Cabin', sans-serif !important;}", type = "text/css"),
-                     tags$style(".fa, .far, .fas {font-family: 'Font Awesome 5 Free' !important;}", type = "text/css")),
+  header = tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "https://fonts.googleapis.com/css?family=Cabin|Oxygen+Mono|Roboto+Mono|Share+Tech+Mono|Source+Code+Pro"),
+    tags$style("* {font-family: 'Cabin', sans-serif !important;}", type = "text/css"),
+    tags$style(".fa, .far, .fas {font-family: 'Font Awesome 5 Free' !important;}", type = "text/css")
+  ),
   id = "main-nav",
   title = "shinySCP",
   theme = shinytheme("yeti"),
@@ -23,31 +25,37 @@ ui <- navbarPage(
   tabPanel(
     title = "Login",
     # Sidebar with a slider input for number of bins
-    fluidRow(column(4),
-             column(4,
-      align = "center",
-      tags$div(class = "well",
-      tags$h2("Login to a server"),
-      tags$br(),
-      textInput("username", label = h4("Username"), value = ""),
-      textInput("server", label = h4("Server"), value = ""),
-      textInput("password", label = h4("Password"), value = ""),
-      actionButton("submit", "Login"),
-      actionButton("reset", "Clear"),
-      tags$br(),
-      tags$br(),
-      textOutput("session"))
-    ),
-    column(4))
+    fluidRow(
+      column(4),
+      column(4,
+        align = "center",
+        tags$div(
+          class = "well",
+          tags$h2("Login to a server"),
+          tags$br(),
+          textInput("username", label = h4("Username"), value = ""),
+          textInput("server", label = h4("Server"), value = ""),
+          textInput("password", label = h4("Password"), value = ""),
+          actionButton("submit", "Login"),
+          actionButton("reset", "Clear"),
+          tags$br(),
+          tags$br(),
+          textOutput("session")
+        )
+      ),
+      column(4)
+    )
   ),
   # About Page
   tabPanel(
+    id = "About",
     title = "About",
     fluidRow(
       column(3),
       column(6,
-      align = "left",
-      tags$div(class = "container-fluid", includeMarkdown("assets/markdown/about.md"))),
+        align = "left",
+        tags$div(class = "container-fluid", includeMarkdown("assets/markdown/about.md"))
+      ),
       column(3)
     )
   )
@@ -70,9 +78,11 @@ server <- function(input, output, session) {
   })
 
   output$session <- renderPrint({
-    if (!is.null(x$username) | !is.null(x$server) | !is.null(x$password)) (
-    connect(x$username, x$server, x$password)
-    )
+    if (!is.null(x$username) | !is.null(x$server) | !is.null(x$password)) {
+      (
+        connect(x$username, x$server, x$password)
+      )
+    }
   })
 }
 
